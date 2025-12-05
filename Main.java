e8d7f163050c808a7c70676d188012adbcc32ee3
Mad Prince <79483029+MadPr1nce@users.noreply.github.com>
2025-12-05 13:29:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int income, vending;
+        income = 44000; vending = 30000;
+
+        System.out.println(income - vending);
+        //second task
+    }
+}
