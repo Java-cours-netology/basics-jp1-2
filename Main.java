cc0a2d81853393d25279d6976929b6998dc6efcd
Mad Prince <79483029+MadPr1nce@users.noreply.github.com>
2025-12-06 02:35:03 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int income, vending;
-        income = 44000; vending = 30000;
-
-        System.out.println(income - vending);
-        //second task
-    }
-}
